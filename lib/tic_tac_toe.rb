772b3038805ac28d8d1617fccc30e2535fd49c0b
Karem Ceron <karemiceron@gmail.com>
2020-05-01 17:11:57 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,7 +98,24 @@ end
    end 
   end
   
-  
+   def winner(board)
+   WIN_COMBINATIONS.each do |win_combination|
+      win_index_0 = win_combination[0]
+      win_index_1 = win_combination[1]
+      win_index_2 = win_combination[2]
+      
+      position_1 = @board[win_index_0]
+      position_2 = @board[win_index_1]
+      position_3 = @board[win_index_2]
+
+      if position_1 == "X" && position_2 == "X" && position_3 == "X"
+        return "X"
+        elsif position_1 == "O" && position_2 == "O" && position_3  == "O"
+        return "O"
+      end
+    end
+    return nil  
+ end
  
  
  
